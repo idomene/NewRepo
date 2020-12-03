78993615c7072356b74807511ca5f5fd0a712f9a
idomene <72223880+idomene@users.noreply.github.com>
2020-12-03 20:16:48 +0100

    Ahorcado

---
+++ practica_2/practica_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --------------------------------------------
+            // Iñaki Domene Castillo
+            // Curso DAM
+            // Modalidad Semipresencial
+            // Práctica nº 2
+            // --------------------------------------------
+
+            //Pido Cadena
+            //Usuario mete cadena
+            //Convierto en char
+            //pido letra
+            //usuario mete letra
+            //recorre el stringbuilder y comprueba si la letra es true o false
+            //si true cambia la letra de la posición, si false suma 1 al contador de errores (muestra el resto de la cadena "_")
+            string cadena;
+            char letra;
+            int i;
+            bool existe = false;
+
+            Console.WriteLine("Cadena: ");
+            cadena = Console.ReadLine();
+
+            StringBuilder cadena2 = new StringBuilder(cadena);
+                cadena2.ToString();
+
+            Console.WriteLine("Introduce una letra: ");
+            letra = Convert.ToChar(Console.ReadLine());
+
+            for (i = 0; i < cadena2.Length; i++)
+            {
+                if (letra == )
+            }
+
+        }
+    }
+}
